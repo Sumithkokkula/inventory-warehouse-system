c999b736d8dd167374d3b949dd4b42ea465fdfd1
Sumithkokkula <sumithkokkula@gmail.com>
2026-08-18 20:22:23 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 DATABASE_URL = "sqlite:///./inventory.db"
 engine = create_engine(
